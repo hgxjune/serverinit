0da711b0ba14ecd3c0ec9c3ed5b012e9433b66df
hgxjune <hgxjune@qq.com>
2020-12-17 15:01:25 +0800

    kerl install erlang script

---
+++ bin/kerl_erlang_path.sh
@@ -0,0 +1,1 @@
+PATH=$PATH:/opt/erlang_17.5.6.10/bin
--- shell/install_erlang17.sh
+++ shell/install_erlang17.sh
@@ -3,3 +3,9 @@
 chmod a+x ../bin/kerl
 ../bin/kerl update releases
 ../bin/kerl build 17.5.6.10 17.5.6.10
+sudo ../bin/kerl install 17.5.6.10 /opt/erlang_17.5.6.10
+# 安装完毕，运行 activate 激活当前环境
+# . /opt/erlang_17.5.6.10/activate
+# 永久激活当前环境
+sudo cp ../bin/kerl_erlang_path.sh /etc/profile.d/kerl_erlang_path.sh
+
