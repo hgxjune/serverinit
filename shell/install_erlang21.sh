d0ac386e9c684550739acd2975365e3c3016312b
hgxjune <hgxjune@qq.com>
2020-12-17 15:55:09 +0800

    erlang-solutions install erlang

--- shell/install_erlang21.sh
+++ shell/install_erlang21.sh
@@ -2,5 +2,6 @@
 
 sudo dpkg -i ../bin/erlang-solutions_2.0_all.deb
 sudo cp ../bin/erlang-solutions.list /etc/apt/sources.list.d/erlang-solutions.list
+sudo cp ../bin/erlang-solutions /etc/apt/preferences.d/erlang-solutions
 sudo apt-cache policy
 
